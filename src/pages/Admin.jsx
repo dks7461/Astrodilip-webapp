cc9adbd082516e2b5b419a9e1fbb12a94cbca46f
Manu Sharma <ansh_s@mt.iitr.ac.in>
2026-05-25 01:21:53 +0530

    Fixed JSX syntax error in Admin page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1298,8 +1298,6 @@ const Admin = () => {
           </div>
         </div>
       )}
-    </div>
-
       {/* Edit Blog Modal */}
       {editBlogData && (
         <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, background: 'rgba(0,0,0,0.5)', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 1000 }}>
